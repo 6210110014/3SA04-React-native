dcc8b211ff84e36a7991a3c8b97f87e63053745d
6210110014 <6210110014@psu.ac.th>
2021-07-18 16:17:50 +0700

    morecontry

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -4,11 +4,13 @@ import { useNavigation } from '@react-navigation/native'
 
 
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+    { place: 'Hatyai', code: '90110',pic:require('../hatyai.jpg') },
+    { place: 'Trang', code: '92000' ,pic:require('../trang.jpg') },
+    { place: 'Chiangmai', code: '50000',pic:require('../Chiangmai.jpg' ) },
+    { place: 'Khonkaen', code: '40000' ,pic:require('../Khonkaen.jpg' )},
+    { place: 'Chonburi', code: '20000',pic:require('../chonburi.jpg' ) },
+    { place: 'Krabi', code: '81000',pic:require('../krabi.jpg')},
+    { place: 'Satun', code: '91000',pic:require('../satun.jpg')},
    ]
    
 
